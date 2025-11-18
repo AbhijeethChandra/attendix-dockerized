20a0437615a2bc387cd9058cbc28a9a57d232c48
abhinavC <abhinav.c@gjglobalsoft.com>
2025-11-18 12:15:36 +0530

    UI created in Shift Master

--- src/routes/privateRoutes.jsx
+++ src/routes/privateRoutes.jsx
@@ -3,11 +3,13 @@ import { Navigate } from "react-router";
 
 const Dashboard = lazy(() => import("@/pages/Dashboard"));
 const Sector = lazy(() => import("@/pages/Sector"));
+const Shift = lazy(() => import("@/pages/Shift"));
 
 export const PrivateRoutes = [
   { index: true, element: <Navigate to="dashboard" /> },
   { path: "dashboard", element: <Dashboard /> },
   { path: "sector-master", element: <Sector /> },
   { path: "office-master", element: <Dashboard /> },
+  { path: "shift-master", element: <Shift /> },
   { path: "*", element: <div>On Development...</div> },
 ];
