6bd01d70932ca202061e72f4bc6ff75f4f801c98
abhinavC <abhinav.c@gjglobalsoft.com>
2025-11-18 11:17:07 +0530

    Ui done in Department Master

--- src/routes/privateRoutes.jsx
+++ src/routes/privateRoutes.jsx
@@ -3,11 +3,13 @@ import { Navigate } from "react-router";
 
 const Dashboard = lazy(() => import("@/pages/Dashboard"));
 const Sector = lazy(() => import("@/pages/Sector"));
+const Department = lazy(() => import("@/pages/Department Master"));
 
 export const PrivateRoutes = [
   { index: true, element: <Navigate to="dashboard" /> },
   { path: "dashboard", element: <Dashboard /> },
   { path: "sector-master", element: <Sector /> },
   { path: "office-master", element: <Dashboard /> },
+  { path: "department-master", element: <Department /> },
   { path: "*", element: <div>On Development...</div> },
 ];
