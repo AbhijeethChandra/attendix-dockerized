6ca801c700bab0b9884a2f92ca33dc27bef782bb
abhinavC <abhinav.c@gjglobalsoft.com>
2025-11-18 13:06:54 +0530

    UI completed in Attendance Request

--- src/routes/privateRoutes.jsx
+++ src/routes/privateRoutes.jsx
@@ -3,11 +3,13 @@ import { Navigate } from "react-router";
 
 const Dashboard = lazy(() => import("@/pages/Dashboard"));
 const Sector = lazy(() => import("@/pages/Sector"));
+const AttendanceRequest = lazy(() => import("@/pages/AttendanceRequest"));
 
 export const PrivateRoutes = [
   { index: true, element: <Navigate to="dashboard" /> },
   { path: "dashboard", element: <Dashboard /> },
   { path: "sector-master", element: <Sector /> },
   { path: "office-master", element: <Dashboard /> },
+  { path: "attendance-request", element: <AttendanceRequest /> },
   { path: "*", element: <div>On Development...</div> },
 ];
