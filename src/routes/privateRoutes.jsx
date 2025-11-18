4d4afd3c062b67b67a796feaf752739eee99c848
abhinavC <abhinav.c@gjglobalsoft.com>
2025-11-18 11:42:54 +0530

    UI created in Employee Master

--- src/routes/privateRoutes.jsx
+++ src/routes/privateRoutes.jsx
@@ -3,11 +3,13 @@ import { Navigate } from "react-router";
 
 const Dashboard = lazy(() => import("@/pages/Dashboard"));
 const Sector = lazy(() => import("@/pages/Sector"));
+const Employee = lazy(() => import("@/pages/Employee"));
 
 export const PrivateRoutes = [
   { index: true, element: <Navigate to="dashboard" /> },
   { path: "dashboard", element: <Dashboard /> },
   { path: "sector-master", element: <Sector /> },
   { path: "office-master", element: <Dashboard /> },
+  { path: "employee-master", element: <Employee /> },
   { path: "*", element: <div>On Development...</div> },
 ];
