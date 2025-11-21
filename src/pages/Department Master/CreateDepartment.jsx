4fe58e07bf276332ff780ea5ca1718efb41e5a7e
abhinavC <abhinav.c@gjglobalsoft.com>
2025-11-21 11:14:53 +0530

    Api integrate in department module

--- src/pages/Department Master/CreateDepartment.jsx
+++ src/pages/Department Master/CreateDepartment.jsx
@@ -1,35 +1,99 @@
 import { Modal } from "@/components/Common/Modal";
 import { CommonInput } from "@/components/Common/CommonInput";
-
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  useCreateDepartmentMutation,
+  useUpdateDepartmentMutation,
+} from "../../app/features/department/departmentApi";
+const INITIAL_STATE = {
+  deptname: "",
+};
 export const CreateDepartment = (props) => {
-  const { isOpen, onClose } = props;
+  const { isOpen, onClose, refetch } = props;
+  const [details, setDetails] = useState(INITIAL_STATE);
+  const user = useSelector((state) => state.auth.user);
+
+  const [createApi, createApiRes] = useCreateDepartmentMutation();
+  const [editApi, editApiRes] = useUpdateDepartmentMutation();
+
+  useEffect(() => {
+    if (typeof isOpen === "object" && isOpen !== null) {
+      setDetails({
+        id: isOpen["-id"],
+        deptname: isOpen.deptname,
+      });
+    } else {
+      setDetails(INITIAL_STATE);
+    }
+  }, [isOpen]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDetails((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const submitData = {
+        ...details,
+        tenantId: user.tenant_id,
+        active: "Y",
+      };
+
+      if (details.id) await editApi(submitData);
+      else await createApi(submitData);
+      setDetails(INITIAL_STATE);
+      refetch();
+      onClose();
+    } catch (err) {
+      console.log("Error creating sector:", err);
+    }
+  };
   return (
     <Modal
       {...{
-        isOpen,
+        isOpen: isOpen ? true : false,
         onClose,
         dialogTitle: "Create Department",
         panelClass: "min-w-[calc(100vw-70vw)]",
         backdropChildClass: "min-h-screen flex items-start justify-end px-4",
       }}
     >
-      <div className="h-[calc(100vh-5rem)] w-full flex flex-col gap-5 px-5 py-3 overflow-y-auto flex flex-col justify-between">
+      <form
+        onSubmit={handleSubmit}
+        className="h-[calc(100vh-5rem)] w-full flex flex-col gap-5 px-5 py-3 overflow-y-auto flex flex-col justify-between"
+      >
         <div className="flex-1 space-y-5">
           <CommonInput
             type="text"
+            required
+            name="deptname"
+            value={details.deptname}
+            onChange={handleChange}
             label="Department Name"
             placeholder="Enter Department Name"
           />
         </div>
         <div className="w-full flex gap-3">
-          <button className="button-1 w-full button-3 rounded-md py-1.5 px-3">
+          <button
+            type="reset"
+            className="button-1 w-full button-3 rounded-md py-1.5 px-3"
+          >
             Reset
           </button>
-          <button className="button-1 w-full rounded-md py-1.5 px-3">
-            Create
+          <button
+            type="submit"
+            className="button-1 w-full rounded-md py-1.5 px-3"
+          >
+            {details.id ? "Update" : "Create"}
           </button>
         </div>
-      </div>
+      </form>
     </Modal>
   );
 };
--- src/pages/Department Master/index.jsx
+++ src/pages/Department Master/index.jsx
@@ -1,12 +1,61 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { CustomTable1 } from "@/components/Common/CustomTable1";
 import { HeadingComp } from "@/components/Common/HeadingComp";
 import { CreateDepartment } from "./CreateDepartment";
+import { useSelector } from "react-redux";
+import { skipToken } from "@reduxjs/toolkit/query";
+import {
+  useGetAllDepartmentQuery,
+  useUpdateStatusDepartmentMutation,
+} from "../../app/features/department/departmentApi";
+import { MdEdit } from "react-icons/md";
+import { twMerge } from "tailwind-merge";
 
 const Department = () => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const user = useSelector((state) => state.auth.user);
+  const [searchText, setSearchText] = useState("");
   const onClose = () => setIsOpen(false);
+
+  const {
+    data: departmentData,
+    isLoading,
+    isError,
+    refetch,
+  } = useGetAllDepartmentQuery(user.tenant_id ?? skipToken);
+
+  const [updateStatusDepartmentApi, updateStatusDepartmentResult] =
+    useUpdateStatusDepartmentMutation();
+
+  const departments = useCallback(() => {
+    if (departmentData?.data.length) {
+      return departmentData.data
+        .filter((data) =>
+          data?.deptname?.toLowerCase().includes(searchText.toLowerCase())
+        )
+        .map((data, index) => ({
+          "-id": data.id,
+          "-active": data.active,
+          sl: index + 1,
+          deptname: data.deptname,
+        }));
+    } else return [];
+  }, [departmentData, searchText]);
+
+  const handleStatus = async (data) => {
+    const updatedStatus = data["-active"] === "Y" ? "N" : "Y";
+    const submitData = {
+      id: data["-id"],
+      active: updatedStatus,
+      tenantId: user.tenant_id,
+    };
+    try {
+      await updateStatusDepartmentApi(submitData);
+      refetch();
+    } catch (err) {
+      console.log("Error updating status:", err);
+    }
+  };
   return (
     <div>
       <HeadingComp
@@ -14,12 +63,41 @@ const Department = () => {
         iconToShow={[]}
         handleButtonClick={() => setIsOpen(true)}
         createButtonText="Create Department"
+        searchValue={searchText}
+        onSearchChange={(e) => setSearchText(e.target.value)}
       />
       <CustomTable1
-        className="table-2"
+        datas={departments()}
         columns={["Sl.No", "Department Name", "Status", "Actions"]}
+        actions={[
+          [
+            ({ data }) => {
+              const isActive = data["-active"] === "Y";
+              return (
+                <button
+                  disabled={updateStatusDepartmentResult.isLoading}
+                  onClick={() => handleStatus(data)}
+                  className={twMerge(
+                    "button-1 rounded-md px-2 py-0.5 button-inactive text-md font-medium",
+                    isActive && "button-active"
+                  )}
+                >
+                  {isActive ? "Active" : "Inactive"}
+                </button>
+              );
+            },
+          ],
+          [
+            ({ data }) => (
+              <MdEdit
+                onClick={() => setIsOpen(data)}
+                className="cursor-pointer size-6 text-[var(--color-icon-2)]"
+              />
+            ),
+          ],
+        ]}
       />
-      <CreateDepartment {...{ isOpen, onClose }} />
+      <CreateDepartment {...{ isOpen, onClose, refetch }} />
     </div>
   );
 };
